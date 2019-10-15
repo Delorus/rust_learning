2cb8e63b74d841cfc313e5da7e3e172d66717ed4
mshherbakov <sherbakov.maks@gmail.com>
2019-10-15 10:33:48 +0500

    functions

---
+++ src/bin/functions.rs
@@ -0,0 +1,15 @@
+fn main() {
+    fn_with_args(1, 2);
+}
+
+fn fn_with_args(x: i32, y: i32) {
+    println!("x = {}, y = {}", x, y);
+}
+
+fn return_value() -> i32 {
+    42
+}
+
+fn not_return_value() -> i32 {
+    42;
+}
